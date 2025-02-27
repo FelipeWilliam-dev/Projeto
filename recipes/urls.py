888c89daeb546fb471efe6ca617877f5fff74eec
Peu <jvggaldino11@gmail.com>
2025-02-27 18:23:43 -0400

    Aula 32-33

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
-from recipes.views import home, contato, sobre #inclui todos as urls
+from recipes.views import home, contato #inclui todos as urls
 #from recipes.views import home, contato, sobre
 
 urlpatterns = [
     path('', home),
     path('contato/', contato),
-    path('sobre/', sobre),
 ]
